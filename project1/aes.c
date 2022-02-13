8d6b5b3fc193e2a5d7bee85e162b43b24f1304d3
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2022-02-13 22:58:56 +0100

    init aes unfinished

---
+++ project1/aes.c
@@ -0,0 +1,97 @@
+#include "aes.h"
+
+void SubBytes(block b, int activeIndexX, int activeIndexY)
+{
+    char left = b[activeIndexX][activeIndexY] & 240; //bitwise AND mask of 1111 0000 to get the left 4 bits of the byte.
+    char right = b[activeIndexX][activeIndexY] & 15; //bitwise AND mask of 0000 1111 to get the remaining 4 bits of the byte.
+    b[activeIndexX][activeIndexY] = S[left >> 4][right];
+}
+
+void InvSubBytes(block b, int activeIndexX, int activeIndexY)
+{
+    char left = b[activeIndexX][activeIndexY] & 240; //bitwise AND mask of 1111 0000 to get the left 4 bits of the byte.
+    char right = b[activeIndexX][activeIndexY] & 15; //bitwise AND mask of 0000 1111 to get the remaining 4 bits of the byte.
+    b[activeIndexX][activeIndexY] = SI[left >> 4][right];
+}
+
+void ShiftRows(block b)
+{
+    unsigned char temp;
+    // Second row
+    temp = b[1][3];
+    b[1][3] = b[1][2];
+    b[1][2] = b[1][1];
+    b[1][0] = temp;
+
+    // Third row
+    temp = b[2][3];
+    b[2][3] = b[2][1];
+    b[2][1] = temp;
+    temp = b[2][2];
+    b[2][2] = b[2][0];
+    b[2][0] = temp;
+
+    // Fourth row
+    temp = b[3][3];
+    b[3][3] = b[3][0];
+    b[3][0] = b[3][1];
+    b[3][1] = b[3][2];
+    b[3][2] = temp;
+}
+
+void InvShiftRows(block b)
+{
+    unsigned char temp;
+    // Second row
+    temp = b[1][3];
+    b[1][3] = b[1][0];
+    b[1][0] = b[1][1];
+    b[1][1] = b[1][2];
+    b[1][2] = temp;
+
+    //Third row
+    temp = b[2][3];
+    b[2][3] = b[2][1];
+    b[2][1] = temp;
+    temp = b[2][2];
+    b[2][2] = b[2][0];
+    b[2][0] = temp;
+
+    // Fourth row
+    temp = b[3][3];
+    b[3][3] = b[3][2];
+    b[3][2] = b[3][1];
+    b[3][1] = b[3][0];
+    b[3][0] = temp;
+}
+
+void MixColumns(block b)
+{
+}
+
+void InvMixColumns(block b)
+{
+}
+
+void AddRoundKey(block b, block roundKey)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            b[i][j] = b[i][j] ^ roundKey[i][j];
+        }
+    }
+}
+
+void KeySchedule(block *schedule, int rounds, block key)
+{
+    block roundKeys[rounds];
+    roundKeys[0] = key;
+    const roundConstant = 1;
+    for (int round = 1; round <= rounds; round++)
+    {
+        block roundKey = key;
+        char b0, b1, b2, b3 = S[]
+    }
+}
