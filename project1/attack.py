da06997be2cf357a57269d78193f3b7b16923c78
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2022-02-16 17:48:17 +0100

    add transform

--- project1/attack.py
+++ project1/attack.py
@@ -4,14 +4,14 @@ from collections import deque
 from typing import List, Set
 
 from sbox import SBOX
-from utils import KEY
+from utils import KEY, create_0_matrix
 from aes import encrypt, add_round_key, shift_rows
 
 
 def create_encrypt_alpha_set(state: List[List[int]] = None, activeLine: int = 0, activeColumn: int = 0) -> List[List[List[int]]]:
     """Create an alpha set filled with 0 and encrypt it."""
     if state is None:
-        state = [[0] * 4] * 4
+        state = create_0_matrix()
     alpha_set = gen_alpha_set(state, activeLine, activeColumn)
     return [encrypt(value, KEY) for value in alpha_set]
 
@@ -44,16 +44,15 @@ def check_guess(state: List[List[List[int]]], activeLine: int = 0, activeColumn:
 
 def get_previous_round_key(round_key: List[List[int]], round: int = 4):
     """Get the round key of the previous round."""
-    previous = [[0] * 4] * 4
+    previous = create_0_matrix()
     for line in range(3, 1, -1):
         for col in range(4):
             previous[line][col] = round_key[line][col] ^ round_key[line-1][col]
     line = deque(previous[3]).rotate(1)
     previous[0] = list(line)
     for i in range(4):
-        value = previous[0][i]
-        previous[0][i] = SBOX[(value & 0b11110000) >> 4][value &
-                                                         0b00001111]
+        byte = previous[0][i]
+        previous[0][i] = SBOX[(byte & 0b11110000) >> 4][byte & 0b00001111]
     previous[0][0] ^= round**2
     for i in range(4):
         previous[0][i] = round_key[0][i] ^ previous[0][i]
@@ -62,13 +61,13 @@ def get_previous_round_key(round_key: List[List[int]], round: int = 4):
 
 def guess_last_round_key(ciphertext: List[List[int]]) -> List[List[int]]:
     """Guess last round key from a ciphertext."""
-    key = [[[]] * 4] * 4
+    key = [[[] for _ in range(4)] for _ in range(4)]
     for i in range(4):
         for j in range(4):
             for byte in range(256):
-                round_key = [[0] * 4] * 4
+                round_key = create_0_matrix()
                 round_key[i][j] = byte
-                state = [[0] * 4] * 4
+                state = create_0_matrix()
                 state[i][j] = reverse_last_round_on_byte(
                     ciphertext, round_key, i, j)
                 alpha_set = gen_alpha_set(state, i, j)
@@ -80,9 +79,10 @@ def guess_last_round_key(ciphertext: List[List[int]]) -> List[List[int]]:
                 test_alpha_set = create_encrypt_alpha_set(
                     activeLine=line, activeColumn=col)
                 for byte in key[i][j]:
-                    round_key = [[0] * 4] * 4
+                    round_key = create_0_matrix()
                     round_key[i][j] = byte
-                    reversed_test_alpha_set = [[[0] * 4] * 4] * 256
+                    reversed_test_alpha_set = [
+                        create_0_matrix() for _ in range(256)]
                     for index, al_set in enumerate(test_alpha_set):
                         reversed_test_alpha_set[index][i][j] = reverse_last_round_on_byte(
                             al_set, round_key, i, j)
