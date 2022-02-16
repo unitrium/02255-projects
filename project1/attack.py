aeddf53e5a3a13addf6e377d52a186b72ccde6c5
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2022-02-16 14:23:03 +0100

    add reverse last round + check guess

--- project1/attack.py
+++ project1/attack.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 from typing import List, Set
 
 from .utils import KEY
-from .aes import encrypt
+from .aes import encrypt, add_round_key, sub_bytes, mix_columns, shift_rows
 
 
 def create_encrypt_alpha_set(state: List[List[int]] = None, activeLine: int = 0, activeColumn: int = 0) -> Set[List[List[int]]]:
@@ -23,6 +23,17 @@ def gen_alpha_set(state: List[List[int]], activeLine: int = 0, activeColumn: int
     return alpha_set
 
 
-def check_guess(state: List[List[int]], round_key: List[List[int]]) -> bool:
+def reverse_last_round_on_byte(ciphertext: List[List[int]], round_key: List[List[int]], activeLine: int = 0, activeColumn: int = 0) -> List[List[int]]:
+    """Reverse the last round on one byte for a given key."""
+    state = add_round_key(ciphertext, round_key)
+    state = shift_rows(ciphertext, inv=True)
+    return state[activeLine][activeColumn]
+
+
+def check_guess(state: List[List[int]], activeColumn: int = 0) -> bool:
     """Checks that a guess on a round key byte is correct."""
-    pass
+    sum = 0
+    # number of matrix in an alpha set = 256
+    for _, value in enumerate(state):
+        sum ^= value[activeColumn]
+    return (sum == 0)
